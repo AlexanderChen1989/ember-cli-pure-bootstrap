7503c34e0c5869b7319994dca75fd9f62b77b141
Alex Chen <alexanderchenmh@gmail.com>
2015-12-30 11:18:29 +0800

    fix

--- blueprints/ember-cli-pure-bootstrap/index.js
+++ blueprints/ember-cli-pure-bootstrap/index.js
@@ -1,6 +1,7 @@
 /*jshint node:true*/
 module.exports = {
   description: 'ember-cli-pure-bootstrap',
+  normalizeEntityName: function() {},
 
   // locals: function(options) {
   //   // Return custom template variables here.
